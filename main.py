f5a3892ed8cb7efbf2032774a8ba60dc8729cb81
ale-stanciu <aleestanciu@gmail.com>
2021-10-13 08:36:05 +0300

    Am rezolvat problema

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def printMenu():
     print ("1. Citire date")
     print ("2. Determinare cea mai lungă subsecvență in care toate numerele sunt pare")
     print ("3. Determinare cea mai lungă subsecvență in care media numerelor nu depășește o valoare citită")
-    print ("4. Iesire")
+    print ("4. Determinare cea mai lunga subsecventa in care toate numerele sunt divizibile cu k")
+    print ("5. Iesire")
 
 def read_list():
     l = []
@@ -81,6 +82,40 @@ def test_get_longest_average_below():
     assert get_longest_average_below([12],5) == []
     assert get_longest_average_below([5,3,10,6],10) == [5,3,10,6]
 
+def EsteDiviziviblCuk(l,k):
+    """
+    Determina daca o lista are toate elementele divizibile cu k
+    :param l: lista de nr. intregi
+    :param k: valoarea citita cu care trebuie sa se divida elementele listei, un nr. intreg
+    :return: True, daca toate elementele sunt divizibile cu k sau False, in caz contrar
+    """
+    for i in l:
+        if i%k!=0:
+            return False
+    return True
+
+def TestEsteDiviziviblCuk():
+    assert EsteDiviziviblCuk([4,10,22,6],2) == True
+    assert EsteDiviziviblCuk([4, 10, 23, 6], 2) == False
+
+def get_longest_div_k(l,k):
+    """
+    Determina cea mai lunga subsecventa in care toate elementele sunt divizibile cu k
+    :param l: lista de nr. intregi
+    :param k: valoarea citita cu care trebuie sa se divida elementele listei, un nr. intreg
+    :return: Cea mai lunga subsecventa in care toate elementele sunt divizibile cu k
+    """
+    subsecventaMax = []
+    for i in range(len(l)):
+        for j in range(i, len(l)):
+            if EsteDiviziviblCuk(l[i:j + 1], k) == True and len(subsecventaMax) < len(l[i:j + 1]):
+                subsecventaMax = l[i:j + 1]
+    return subsecventaMax
+
+def test_get_longest_div_k():
+    assert get_longest_div_k([5,4,16,8,3],4) == [4,16,8]
+    assert get_longest_div_k([2,6,15,20], 7) == []
+    assert get_longest_div_k([3,9,18,6], 3) == [3,9,18,6]
 
 def main():
     test_get_longest_all_even()
@@ -96,6 +131,9 @@ def main():
             average = int(input("Dati valoarea cu care sa se compare media:"))
             print(get_longest_average_below(l, average))
         elif optiune =="4":
+            k = int(input("Dati valoarea cu care doriti sa fie divizibile numelere: "))
+            print(get_longest_div_k(l,k))
+        elif optiune == "5":
             break
         else:
             print("Optiune gresita! Reincercati!")
